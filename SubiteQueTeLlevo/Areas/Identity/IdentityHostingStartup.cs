1b69399f6694c8441b0182034ae0dc4bb1fc43c7
AgusMaris <agustinm06@gmail.com>
2021-01-22 01:53:58 -0300

    Fix css

---
+++ SubiteQueTeLlevo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SubiteQueTeLlevo.Data;
+
+[assembly: HostingStartup(typeof(SubiteQueTeLlevo.Areas.Identity.IdentityHostingStartup))]
+namespace SubiteQueTeLlevo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
